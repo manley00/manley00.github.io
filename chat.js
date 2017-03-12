272dd8167006ad97c7d1085269a45fe16481eb08
Manley Celicourt <manleycelicourt@yahoo.fr>
2017-03-12 03:48:44 +0000

    Add styling to personal website

--- chat.js
+++ chat.js
@@ -1,35 +1,19 @@
-function messageSend() {
-    var c = new Date();
-    var h = c.getHours();
-    var m = c.getMinutes();
-    m = checkTime(m);
-    var s = c.getSeconds();
-    s = checkTime(s);
-    var n = document.getElementById("nameInputBox").value;
-    var msg = document.getElementById("messageOutputBox").innerHTML; 
-    var value_msg = document.getElementById("messageInputBox").value;
-    
-    if (value_msg != "") document.getElementById("messageOutputBox").innerHTML  =
-    msg+"["+h+":"+m+":"+s+"] "+" ["+n+"]"+value_msg+"<br />"+"<hr/>";
-       
+var config = {
+ apiKey: "AIzaSyB4FRUXGcWqn9D1kwz0x_utFad8_BkFYqs",
+ authDomain: "chat-app-76571.firebaseapp.com",
+ databaseURL: "https://chat-app-76571.firebaseio.com",
+ storageBucket: "chat-app-76571.appspot.com",
+ messagingSenderId: "951173134018"
+ };
+ firebase.initializeApp(config);
+ 
+ var chatData = firebase.database().ref();
+ 
+ function pushMessage(event) {
+     if (event.keyCode == 13) {
+    var name = $('#nameInput').val();
+    var text = $('#messageInput').val();
+    chatData.push({name: name, text: text});
+    $('#messageInput').val('');
 }
-
-function chatBoxTime() {
-    var c2 = new Date();
-    var h2 = c2.getHours();
-    var m2 = c2.getMinutes();
-    m2 = checkTime(m2);
-    var s2 = c2.getSeconds();
-    s2 = checkTime(s2);
-    
-    document.getElementById("clockDisplay").innerHTML = 
-    "Mchat v1.0 By: Manley.C"+"\xa0\xa0\xa0\xa0"
-    +"["+h2+":"+m2+":"+s2+"]";
-}
-
-setInterval(chatBoxTime, 1000);
-
-function checkTime(v) {
-    if (v < 10) {v = "0" +v}
-    return v;
-}
+$('#messageInput').keypress(pushMessage');
